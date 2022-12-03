3b3e075d75fd57627b617b1f36f91aaf660f1ddd
Vadym Kohut <vadym.kohut@gmail.com>
2022-12-03 20:58:17 +0100

    ng material added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,21 +8,20 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { ChosenIngredientsComponent } from './chosen-ingredients/chosen-ingredients.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { RandomCocktailListComponent } from './random-cocktail-list/random-cocktail-list.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    SidebarComponent,
-    ChosenIngredientsComponent,
-    PageNotFoundComponent,
-    RandomCocktailListComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        SidebarComponent,
+        ChosenIngredientsComponent,
+        PageNotFoundComponent,
+        RandomCocktailListComponent,
+    ],
+    imports: [BrowserModule, AppRoutingModule, MatGridListModule, BrowserAnimationsModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
