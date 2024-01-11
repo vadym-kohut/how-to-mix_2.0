1218d37b9a3b1a4148739b90a593c8308b2329fb
Vadym Kohut <vadym.kohut@gmail.com>
2024-01-11 20:33:45 +0100

    configuring spinner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { MatGridListModule } from "@angular/material/grid-list";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatListModule } from "@angular/material/list";
 import { MatIconModule } from "@angular/material/icon";
-import { HttpClientModule } from "@angular/common/http";
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
 import { StoreModule } from "@ngrx/store";
 import { EffectsModule } from "@ngrx/effects";
 import { StoreDevtoolsModule } from "@ngrx/store-devtools";
@@ -18,6 +18,9 @@ import { appReducer } from "./state/app.state";
 import { CommonModule } from "@angular/common";
 import { ChosenIngComponent } from "./chosen-ingredients/chosen-ing/chosen-ing.component";
 import { environment } from "../environments/environment";
+import { SpinnerComponent } from "./shared/components/spinner/spinner.component";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { LoaderInterceptor } from "./shared/interceptors/loader.interceptor";
 
 @NgModule({
     declarations: [
@@ -26,7 +29,8 @@ import { environment } from "../environments/environment";
         SidebarComponent,
         ChosenIngredientsComponent,
         PageNotFoundComponent,
-        ChosenIngComponent
+        ChosenIngComponent,
+        SpinnerComponent
     ],
     imports: [
         CommonModule,
@@ -43,9 +47,16 @@ import { environment } from "../environments/environment";
             name: "HowToMix App Devtools",
             maxAge: 25,
             logOnly: environment.production
-        })
+        }),
+        MatProgressSpinnerModule
     ],
-    providers: [],
+    providers: [
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: LoaderInterceptor,
+            multi: true
+        }
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule {
