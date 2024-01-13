ae3265d3378e6f35e4a668064733c93b74ec5a6a
Vadym Kohut <vadym.kohut@gmail.com>
2024-01-13 19:27:21 +0100

    cocktail card standalone component created

--- src/app/pages/cocktails/cocktails.module.ts
+++ src/app/pages/cocktails/cocktails.module.ts
@@ -5,24 +5,24 @@ import { CocktailsRoutingModule } from "./cocktails-routing.module";
 import { CocktailsComponent } from "./cocktails.component";
 import { CocktailListComponent } from "./cocktail-list/cocktail-list.component";
 import { CocktailDetailsComponent } from "./cocktail-details/cocktail-details.component";
-import { CocktailCardComponent } from "./cocktail-list/cocktail-card/cocktail-card.component";
 import { CocktailsByIngredientComponent } from "./cocktails-by-ingredient/cocktails-by-ingredient.component";
 import { StoreModule } from "@ngrx/store";
 import { cocktailsReducer } from "./state/cocktails.reducer";
 import { EffectsModule } from "@ngrx/effects";
 import { CocktailsEffects } from "./state/cocktails.effects";
+import { CocktailCardComponent } from "../../shared/components/cocktail-card/cocktail-card.component";
 
 @NgModule({
     declarations: [
         CocktailsComponent,
         CocktailListComponent,
         CocktailDetailsComponent,
-        CocktailCardComponent,
         CocktailsByIngredientComponent
     ],
     imports: [
         CommonModule,
         CocktailsRoutingModule,
+        CocktailCardComponent,
         StoreModule.forFeature("cocktails", cocktailsReducer),
         EffectsModule.forFeature([CocktailsEffects])
     ]
---
+++ src/app/shared/components/cocktail-card/cocktail-card.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from "@angular/core";
+import { CocktailDetails } from "../../models/cocktail.model";
+
+@Component({
+    selector: "htm-cocktail-card",
+    templateUrl: "./cocktail-card.component.html",
+    styleUrls: ["./cocktail-card.component.scss"],
+    standalone: true
+})
+export class CocktailCardComponent {
+    @Input() cocktailDetails!: CocktailDetails;
+}
