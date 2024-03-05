ac3c85db42a112d5a0c7fb3f0bf0381beb9af4ee
Vadym Kohut <vadym.kohut@gmail.com>
2024-03-05 18:52:56 +0100

    favourite cocktail list: state configured

--- src/app/pages/cocktails/state/cocktails.actions.ts
+++ src/app/pages/cocktails/state/cocktails.actions.ts
@@ -1,6 +1,7 @@
 import { createAction, props } from "@ngrx/store";
 import { CocktailDetails, CocktailListItem } from "../../../shared/models/cocktail.model";
 
+// LOAD COCKTAIL LIST
 export const loadCocktailListByFirstLetter = createAction(
     "[Cocktails] Load Cocktail List By First Letter",
     props<{ firstLetter: string }>()
@@ -11,6 +12,7 @@ export const loadCocktailListByFirstLetterSuccess = createAction(
     props<{ cocktailList: CocktailDetails[] }>()
 );
 
+// LOAD COCKTAIL LIST BY INGREDIENT
 export const loadCocktailListByIngredient = createAction(
     "[Cocktails] Load Cocktail List By Ingredient",
     props<{ ingredientName: string }>()
@@ -21,6 +23,7 @@ export const loadCocktailListByIngredientSuccess = createAction(
     props<{ cocktailList: CocktailListItem[] }>()
 );
 
+// LOAD COCKTAIL DETAILS
 export const loadCocktailDetails = createAction(
     "[Cocktails] Load Cocktail Details",
     props<{ id: string }>()
@@ -29,4 +32,19 @@ export const loadCocktailDetails = createAction(
 export const loadCocktailDetailsSuccess = createAction(
     "[Cocktails] Load Cocktail Details Success",
     props<{ cocktailDetails: CocktailDetails }>()
+);
+
+// FAVOURITE COCKTAIL LIST
+export const addToFavouriteCocktailList = createAction(
+    "[Cocktails] Add To Favourite Ingredient List",
+    props<{ cocktailName: string }>()
+);
+
+export const removeFromFavouriteCocktailList = createAction(
+    "[Cocktails] Remove From Favourite Ingredient List",
+    props<{ cocktailToRemoveName: string }>()
+);
+
+export const clearFavouriteCocktailList = createAction(
+    "[Cocktails] Clear Favourite Ingredient List"
 );
--- src/app/pages/cocktails/state/cocktails.reducer.ts
+++ src/app/pages/cocktails/state/cocktails.reducer.ts
@@ -11,12 +11,14 @@ export interface CocktailState {
     cocktailListByFirstLetter: CocktailDetails[];
     cocktailListByIngredient: CocktailListItem[];
     cocktailDetails: CocktailDetails | null;
+    favouriteCocktailList: string[];
 }
 
 const initialState: CocktailState = {
     cocktailListByFirstLetter: [],
     cocktailListByIngredient: [],
-    cocktailDetails: null
+    cocktailDetails: null,
+    favouriteCocktailList: []
 };
 
 // Selectors
@@ -26,35 +28,60 @@ export const getCocktailsByFirstLetter = createSelector(
     getCocktailFeatureState,
     state => state.cocktailListByFirstLetter
 );
-
 export const getCocktailListByIngredient = createSelector(
     getCocktailFeatureState,
     state => state.cocktailListByIngredient
 );
-
 export const getCocktailDetails = createSelector(
     getCocktailFeatureState,
     state => state.cocktailDetails
 );
+export const getFavouriteCocktailList = createSelector(
+    getCocktailFeatureState,
+    state => state.favouriteCocktailList
+);
 
 export const cocktailsReducer = createReducer(
     initialState,
+    // LOAD COCKTAIL LIST
     on(CocktailActions.loadCocktailListByFirstLetterSuccess, (state, { cocktailList }): CocktailState => {
         return {
             ...state,
             cocktailListByFirstLetter: cocktailList
         };
     }),
+    // LOAD COCKTAIL LIST BY INGREDIENT
     on(CocktailActions.loadCocktailListByIngredientSuccess, (state, { cocktailList }): CocktailState => {
         return {
             ...state,
             cocktailListByIngredient: cocktailList
         };
     }),
+    // LOAD COCKTAIL DETAILS
     on(CocktailActions.loadCocktailDetailsSuccess, (state, { cocktailDetails }): CocktailState => {
         return {
             ...state,
             cocktailDetails: cocktailDetails
+        };
+    }),
+    // FAVOURITE COCKTAIL LIST
+    on(CocktailActions.addToFavouriteCocktailList, (state, { cocktailName }): CocktailState => {
+        return state.favouriteCocktailList.includes(cocktailName) ? state :
+            {
+                ...state,
+                favouriteCocktailList: [...state.favouriteCocktailList, cocktailName]
+            };
+    }),
+    on(CocktailActions.removeFromFavouriteCocktailList, (state, { cocktailToRemoveName }): CocktailState => {
+        return {
+            ...state,
+            favouriteCocktailList: state.favouriteCocktailList.filter(cocktailName => cocktailName !== cocktailToRemoveName)
+        };
+    }),
+    on(CocktailActions.clearFavouriteCocktailList, (state): CocktailState => {
+        return {
+            ...state,
+            favouriteCocktailList: []
         };
     })
 );
